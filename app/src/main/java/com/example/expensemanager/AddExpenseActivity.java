ea4a8073c19f22e752bcaeba89170fd3053d5c61
Shrestha <shresthabanerjee@gmail.com>
2023-10-07 02:29:29 +0530

    basic xml done

--- app/src/main/java/com/example/expensemanager/AddExpenseActivity.java
+++ app/src/main/java/com/example/expensemanager/AddExpenseActivity.java
@@ -1,8 +1,12 @@
 package com.example.expensemanager;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.example.expensemanager.databinding.ActivityMainBinding;
 
@@ -13,5 +17,25 @@ public class AddExpenseActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater=getMenuInflater();
+        menuInflater.inflate(R.menu.add_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id=item.getItemId();
+        if(id==R.id.saveExpense){
+            createExpense();
+            return true;
+        }
+        return false;
+    }
+
+    private void createExpense() {
     }
 }
