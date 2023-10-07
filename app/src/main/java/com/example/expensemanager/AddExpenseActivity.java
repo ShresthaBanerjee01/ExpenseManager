c9a4dfd14f85744e5d1d9ae2fc52f12866d48442
Shrestha <shresthabanerjee@gmail.com>
2023-10-07 23:46:00 +0530

    java file started

--- app/src/main/java/com/example/expensemanager/AddExpenseActivity.java
+++ app/src/main/java/com/example/expensemanager/AddExpenseActivity.java
@@ -7,15 +7,21 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.widget.RadioButton;
 
+import com.example.expensemanager.databinding.ActivityAddExpenseBinding;
 import com.example.expensemanager.databinding.ActivityMainBinding;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Calendar;
+import java.util.UUID;
 
 public class AddExpenseActivity extends AppCompatActivity {
-    ActivityMainBinding binding;
+    @NonNull ActivityAddExpenseBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        binding = ActivityAddExpenseBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
     }
 
@@ -37,5 +43,32 @@ public class AddExpenseActivity extends AppCompatActivity {
     }
 
     private void createExpense() {
+
+        String expenseId = UUID.randomUUID().toString();
+        String amount = findViewById(R.id.amount).toString();
+        String note = findViewById(R.id.note).toString();
+        String category = findViewById(R.id.category).toString();
+        String type;
+
+        boolean incomeChecked = binding.incomeRadio.isChecked();
+
+        if(incomeChecked) {
+            type="Income";
+        }else{
+            type="Expense";
+        }
+        if(amount.trim().length()==0){
+            binding.amount.setError("Empty");
+            return;
+        }
+        ExpenseModel expenseModel=new ExpenseModel(expenseId,note,category,type,Long.parseLong(amount), Calendar.getInstance().getTimeInMillis());
+
+        FirebaseFirestore
+                .getInstance()
+                .collection("expenses")
+                .document(expenseId)
+                .set(expenseModel);
+        finish();
+
     }
 }
